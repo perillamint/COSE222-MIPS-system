7d078745037df6359cef4c260278bbdd146c5090
perillamint <perillamint@quendi.moe>
2019-11-11 02:20:59 +0900

    Implement JR

--- MIPS_CPU/mips.v
+++ MIPS_CPU/mips.v
@@ -18,7 +18,7 @@ module mips(input         clk, reset,
 
   wire        signext, shiftl16, memtoreg, branch;
   wire        pcsrc, zero;
-  wire        alusrc, regdst, regwrite, nez, jump, link;
+  wire        alusrc, regdst, regwrite, nez, jump, link, jumptoreg;
   wire [2:0]  alucontrol;
 
   // Instantiate Controller
@@ -37,6 +37,7 @@ module mips(input         clk, reset,
 		.jump       (jump),
     .nez        (nez),
     .link       (link),
+    .jumptoreg  (jumptoreg),
 		.alucontrol (alucontrol));
 
   // Instantiate Datapath
@@ -53,6 +54,7 @@ module mips(input         clk, reset,
     .jump       (jump),
     .nez        (nez),
     .link       (link),
+    .jumptoreg  (jumptoreg),
     .alucontrol (alucontrol),
     .zero       (zero),
     .pc         (pc),
@@ -73,6 +75,7 @@ module controller(input  [5:0] op, funct,
                   output       jump,
                   output       nez,
                   output       link,
+                  output       jumptoreg,
                   output [2:0] alucontrol);
 
   wire [1:0] aluop;
@@ -80,6 +83,7 @@ module controller(input  [5:0] op, funct,
 
   maindec md(
     .op       (op),
+    .funct    (funct),
     .signext  (signext),
     .shiftl16 (shiftl16),
     .memtoreg (memtoreg),
@@ -91,6 +95,7 @@ module controller(input  [5:0] op, funct,
     .jump     (jump),
     .link     (link),
     .nez      (nez),
+    .jumptoreg(jumptoreg),
     .aluop    (aluop));
 
   aludec ad(
@@ -104,37 +109,45 @@ endmodule
 
 
 module maindec(input  [5:0] op,
+               input [5:0]  funct,
                output       signext,
                output       shiftl16,
                output       memtoreg, memwrite,
                output       branch, alusrc,
                output       regdst, regwrite,
                output       jump,
+               output       jumptoreg,
                output       link,
                output       nez,
                output [1:0] aluop);
 
-  reg [12:0] controls;
+  reg [13:0] controls;
 
   assign {signext, shiftl16, regwrite, regdst, alusrc, branch, memwrite,
-          memtoreg, jump, link, nez, aluop} = controls;
+          memtoreg, jump, link, jumptoreg, nez, aluop} = controls;
 
   always @(*)
     case(op)
-      6'b000000: controls <= #`mydelay 13'b0011000000011; // Rtype
-      6'b100011: controls <= #`mydelay 13'b1010100100000; // LW
-      6'b101011: controls <= #`mydelay 13'b1000101000000; // SW
-      6'b000100: controls <= #`mydelay 13'b1000010000001; // BEQ
-      6'b000101: controls <= #`mydelay 13'b1000010000101; // BNE
+      6'b000000:
+        begin
+           case (funct)
+             6'b001000: controls <= #`mydelay 14'b00000000001000; // JR
+             default:   controls <= #`mydelay 14'b00110000000011; // Rtype
+           endcase // case (funct)
+        end
+      6'b100011: controls <= #`mydelay 14'b10101001000000; // LW
+      6'b101011: controls <= #`mydelay 14'b10001010000000; // SW
+      6'b000100: controls <= #`mydelay 14'b10000100000001; // BEQ
+      6'b000101: controls <= #`mydelay 14'b10000100000101; // BNE
       6'b001000,
-      6'b001001: controls <= #`mydelay 13'b1010100000000; // ADDI, ADDIU: only difference is exception
+      6'b001001: controls <= #`mydelay 14'b10101000000000; // ADDI, ADDIU: only difference is exception
       6'b001010,
-      //6'b001011: controls <= #`mydelay 13'b1011000000000; // SLTI, SLTIU
-      6'b001101: controls <= #`mydelay 13'b0010100000010; // ORI
-      6'b001111: controls <= #`mydelay 13'b0110100000000; // LUI
-      6'b000010: controls <= #`mydelay 13'b0000000010000; // J
-      6'b000011: controls <= #`mydelay 13'b0010000011000; // JAL
-      default:   controls <= #`mydelay 13'bxxxxxxxxxxxxx; // ???
+      //6'b001011: controls <= #`mydelay 14'b10110000000000; // SLTI, SLTIU
+      6'b001101: controls <= #`mydelay 14'b00101000000010; // ORI
+      6'b001111: controls <= #`mydelay 14'b01101000000000; // LUI
+      6'b000010: controls <= #`mydelay 14'b00000000100000; // J
+      6'b000011: controls <= #`mydelay 14'b00100000110000; // JAL
+      default:   controls <= #`mydelay 14'bxxxxxxxxxxxxxx; // ???
     endcase
 
 endmodule
@@ -171,6 +184,7 @@ module datapath(input         clk, reset,
                 input         regwrite, jump,
                 input         nez,
                 input         link,
+                input         jumptoreg,
                 input [2:0]   alucontrol,
                 output        zero,
                 output [31:0] pc,
@@ -184,6 +198,7 @@ module datapath(input         clk, reset,
   wire [31:0] srca, srcb;
   wire [31:0] aluordata;
   wire [31:0] result;
+  wire [31:0] jraddr;
   wire        shift;
 
   // next PC logic
@@ -213,8 +228,14 @@ module datapath(input         clk, reset,
     .s   (pcsrc),
     .y   (pcnextbr));
 
+  mux2 #(32) jrmux(
+    .d0  (pcnextbr),
+    .d1  (srca),
+    .s   (jumptoreg),
+    .y   (jraddr));
+
   mux2 #(32) pcmux(
-    .d0   (pcnextbr),
+    .d0   (jraddr),
     .d1   ({pcplus4[31:28], instr[25:0], 2'b00}),
     .s    (jump),
     .y    (pcnext));
