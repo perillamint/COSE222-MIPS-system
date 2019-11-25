fa420d492ec9e752cfaf78af5ce352aaf905dcd2
perillamint <perillamint@quendi.moe>
2019-11-25 15:05:45 +0900

    Redesign IF

--- MIPS_CPU/pipeline/if.v
+++ MIPS_CPU/pipeline/if.v
@@ -8,27 +8,19 @@ module pipeline_if(input clk, reset,
                    input  [31:0] offset,
                    output [31:0] pc);
 
-   wire [31:0]                   pcnext;
-   wire [31:0]           srca, srcb;
-   wire [31:0]           jraddr;
+   wire [31:0]           pcplus4, pcnext;
 
-   // next PC logic
    flopr #(32) pcreg(.clk   (clk),
                      .reset (reset),
                      .d     (pcnext),
                      .q     (pc));
 
-   mux2 #(32) pcnextmux(.d0  (32'b100),
-                        .d1  (offset),
-                        .s   (pcsrc),
-                        .y   (pcnextoff));
-
-   mux2 #(32) pchazardmux(.d0  (nextoff),
-                          .d1  (32'b0),
-                          .s   (hazard),
-                          .y   (pcadd));
-
    adder pcadd1(.a (pc),
-                .b (pcadd),
-                .y (pcnext));
+                .b (32'b100),
+                .y (pcplus4));
+
+   mux2 #(32) pcsrcmux(.d0  (pcplus4),
+                       .d1  (offset),
+                       .s   (pcsrc),
+                       .y   (pcnext));
 endmodule // pipeline_if
